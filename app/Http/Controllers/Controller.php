1dde1e2dd810513d5c768299909715282628fcf6
Tony ID <tony.inuma@gmail.com>
2021-11-29 11:38:40 -0500

    Feat: Swagger Docs. To Api documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,55 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    /**
+     * @OA\Info(
+     *     title="TestUsil2021",
+     *     version="1.0.0"
+     * )
+     *
+     * @OA\SecurityScheme(
+     *     type="http",
+     *     description="login",
+     *     name="token",
+     *     in="header",
+     *     scheme="bearer",
+     *     bearerFormat="JWT",
+     *     securityScheme="apiAuth"
+     * )
+     *
+     * @OA\Server(
+     *      url="http://testusil2021.tk/api",
+     *      description="Test Usil",
+     *
+     * @OA\ServerVariable(
+     *      serverVariable="schema",
+     *      enum={"https", "http"},
+     *      default="http"
+     *  )
+     * )
+     *
+     * @OA\Server(
+     *      url="http://comicomerce.com/api",
+     *      description="comicomerce.com",
+     *
+     * @OA\ServerVariable(
+     *      serverVariable="schema",
+     *      enum={"http"},
+     *      default="http"
+     *  )
+     * )
+     * @OA\Server(
+     *      url="http://127.0.0.1/api",
+     *      description="localhost",
+     *
+     * @OA\ServerVariable(
+     *      serverVariable="schema",
+     *      enum={"http"},
+     *      default="http"
+     *  )
+     * )
+     *
+     * )
+     */
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
