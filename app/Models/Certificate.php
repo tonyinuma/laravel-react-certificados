3a16163cdde4eb16eac6fe28951ead5fa8ba23f2
Tony ID <tony.inuma@gmail.com>
2021-11-29 09:00:46 -0500

    Add: certificates and courses models

---
+++ app/Models/Certificate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Certificate extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'courses';
+
+    protected $fillable = [
+        'course_id',
+        'name',
+        'certificate_date'
+    ];
+
+    protected $dates = [
+        'certificate_date',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
---
+++ app/Models/Course.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Course extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'courses';
+
+    protected $fillable = [
+        'name',
+        'active'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
